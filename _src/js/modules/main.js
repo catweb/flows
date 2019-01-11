6fd7753e8378f6087274fb7e3e13ff8997cb8d60
catweb <pozdyshev.vladimir@gmail.com>
2019-01-11 16:32:34 +0300

    drag appearance, code optimization

--- _src/js/modules/main.js
+++ _src/js/modules/main.js
@@ -1,103 +1,88 @@
 class FlowsCanvas {
-  constructor(canvas){
+  constructor(canvas) {
     const that = this;
+    const props = {
+      arrow: {
+        location: 1,
+        visible: true,
+        width: 5,
+        length: 10,
+        id: "ARROW"
+      },
+      flowchart: {
+        gap: 0,
+        cornerRadius: 8
+      }
+    };
     const jsp = this.jsp = jsPlumb.getInstance({
-      Connector: ["Flowchart",
-        {
-          gap: 0,
-          cornerRadius: 4
-        }
-      ],
+      Connector: ["Flowchart", props.flowchart],
       ConnectionOverlays: [
-        ["Arrow", {
-          location: 1,
-          visible: true,
-          width: 5,
-          length: 10,
-          id: "ARROW"
-        }]
+        ["Arrow", props.arrow]
       ],
-      PaintStyle: {stroke: "#808080", strokeWidth: 2, outlineStroke: "transparent", outlineWidth: 4},
+      PaintStyle: {stroke: "#808080", strokeWidth: 2, outlineStroke: "transparent", outlineWidth: 5},
       Container: canvas
     });
+
     jsp.registerConnectionTypes({
-      "basic": {
-        endpoints: [
-          ["Blank", {}],
-          ["Rectangle", {width: 10, height: 10, cssClass:'flows-endpoint'}]
-        ],
-        anchors: [["Right"], ["Continuous", { faces:[ "top", "left", "bottom" ] }]],
-        connector: ["Flowchart",
-          {
-            gap: 0,
-            cornerRadius: 8
-          }
-        ],
-        paintStyle: {stroke: "#808080", strokeWidth: 2, outlineStroke: "transparent", outlineWidth: 4},
-        overlays: [
-          ["Arrow", {width: 5, length: 10, location: 1, visible: true}]
-        ]
-      },
       "drag": {
-        endpoints: [
-          ["Blank", {}],
-          ["Blank", {}]
-        ],
-        connector: ["Straight"],
         paintStyle: {stroke: "#2e0085", strokeWidth: 2},
-        overlays: [
-          ["Arrow", {width: 5, length: 10, location: 1, visible: true}]
-        ]
       },
       "selected": {
         paintStyle: {stroke: "#0478ff", strokeWidth: 2},
       }
     });
 
-    jsp.bind('connection', function(info){
+    jsp.bind('connection', function (info) {
       let connection = info.connection;
       let arr = jsp.select({
         source: connection.sourceId,
         target: connection.targetId
       });
-      if(arr.length > 1 || $(connection.target).has(connection.source).length){
+      if (arr.length > 1 || $(connection.target).has(connection.source).length) {
         jsp.deleteConnection(connection);
       }
       else {
-        jsp.fire('connectionAdd', connection);
-
         connection.unbind('click');
         connection.bind("click", function () {
-          if(connection.hasType('selected')){
+          if (connection.hasType('selected')) {
             connection.removeType("selected");
           }
           else {
-            jsp.getAllConnections().forEach(function (conn) {
-              conn.removeType('selected')
-            });
+            for (let i = 0, all = jsp.getAllConnections(), allLength = all.length; i < allLength; i++) {
+              if (all[i].hasType('selected')) {
+                all[i].removeType('selected');
+                break;
+              }
+            }
             connection.addType("selected");
           }
+          connection.removeOverlay(props.arrow.id);
+          connection.addOverlay(["Arrow", props.arrow]);
         });
+
+        jsp.fire('connectionAdd', connection);
       }
     });
 
     jsp.bind("connectionDrag", function (connection) {
-      // connection.removeType("basic");
-      // connection.addType("drag");
+      connection.addType("drag");
+      connection.setConnector(["Straight"]);
+      connection.addOverlay(["Arrow", props.arrow]);
       connection.addClass('flows-drag');
     });
 
     jsp.bind("connectionDragStop", function (connection, origEvent) {
-      // connection.removeType("drag");
-      // connection.addType("basic");
+      connection.removeType("drag");
+      connection.setConnector(["Flowchart", props.flowchart]);
+      connection.addOverlay(["Arrow", props.arrow]);
       connection.removeClass('flows-drag');
     });
 
     $(document).on('keyup', function (e) {
       //deleting selected connection on press DEL
-      if(e.keyCode === 46){
-        for(let i = 0, all = jsp.getAllConnections(), allLength = all.length; i < allLength; i++) {
-          if(all[i].hasType('selected')){
+      if (e.keyCode === 46) {
+        for (let i = 0, all = jsp.getAllConnections(), allLength = all.length; i < allLength; i++) {
+          if (all[i].hasType('selected')) {
             that.deleteConnection(all[i]);
             break;
           }
@@ -106,25 +91,24 @@ class FlowsCanvas {
     });
   }
 
-  deleteConnection(connection){
+  deleteConnection(connection) {
     this.jsp.deleteConnection(connection);
     this.jsp.fire('connectionDelete', connection);
   }
 
-  connect(source, target){
+  connect(source, target) {
     this.jsp.connect({
       source: source,
       target: target
     });
   }
 
-  makeItem(elem){
+  makeItem(elem) {
     let that = this;
-    let jsp = this.jsp;
     this.jsp.makeTarget(elem, {
-      anchor: ["Continuous", { faces:[ "top", "left", "bottom" ] }],
+      anchor: ["Continuous", {faces: ["top", "left", "bottom"]}],
       endpoint: ["Rectangle", {width: 10, height: 10, cssClass: 'flows-endpoint'}],
-      paintStyle:{fill: "blue"}
+      paintStyle: {fill: "blue"}
     });
     this.jsp.draggable(elem, {
       handle: '.flows-box__header__handle',
@@ -133,7 +117,7 @@ class FlowsCanvas {
       }
     });
     elem.find('.flows-box__output').each(function () {
-      jsp.makeSource($(this), {
+      that.jsp.makeSource($(this), {
         anchor: "Right",
         endpoint: 'Blank'
       });
@@ -142,11 +126,11 @@ class FlowsCanvas {
       that.deleteItem(elem);
     });
     elem.on('mousedown', function () {
-      if(elem.hasClass('flows-active')){
+      if (elem.hasClass('flows-active')) {
         elem.removeClass('flows-active');
       }
       else {
-        let container = jsp.getContainer();
+        let container = that.jsp.getContainer();
         $(container)
           .find('.flows-active')
           .removeClass('flows-active');
@@ -155,7 +139,7 @@ class FlowsCanvas {
     });
   }
 
-  deleteItem(elem){
+  deleteItem(elem) {
     this.jsp.remove(elem);
     this.jsp.fire('itemDelete', elem);
   }
@@ -174,14 +158,14 @@ jsPlumb.ready(function () {
       console.log('connectionDelete:', connection);
     });
     canvas.jsp.bind('connectionAdd', function (connection) {
-      console.log('connectionAdd:', connection);
+      // console.log('connectionAdd:', connection);
     });
     canvas.jsp.bind('itemDelete', function (elem) {
       console.log('itemDelete:', elem);
     });
 
-    canvas.connect('box1out1','box2');
-    canvas.connect('box3out1','box2');
+    canvas.connect('box1out1', 'box2');
+    canvas.connect('box3out1', 'box2');
 
   });
 });
